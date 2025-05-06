4d5f78b35dabaaeb52251b0e3da1384ea4ad9392
destinyFvcker <destinyfunk@163.com>
2025-05-06 14:03:42 +0800

    test: proto github action build

--- build.rs
+++ build.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use prost_build;
+
+fn main() {
+    prost_build::Config::new()
+        .compile_protos(&["proto/kafka_alarm.proto", "proto/ws.proto"], &["proto/"])
+        .unwrap();
+}
