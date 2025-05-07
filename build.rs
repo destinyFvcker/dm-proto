1af78b467d307c10af8ca381a8ddd9165cda983b
destinyFvcker <destinyfunk@163.com>
2025-05-07 18:10:01 +0800

    feat: add alarm log phase1

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use prost_build;
 
 fn main() {
     prost_build::Config::new()
+        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .compile_protos(&["proto/kafka_alarm.proto", "proto/ws.proto"], &["proto/"])
         .unwrap();
 }
